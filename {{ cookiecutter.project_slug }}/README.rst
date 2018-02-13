ed779c42a91849864e0299ba3397e786a2a5bc97
Sergei Pogorelov <pogorelov.ss@gmail.com>
2018-02-13 14:04:14 +0200

    init

---
+++ {{ cookiecutter.project_slug }}/README.rst
@@ -0,0 +1,21 @@
+
+JWT authentication for Django based multi-tenant (microservices)  services
+
+tox framework for building, py.test and pytest-cov for tests
+
+`tox -r` - rebuild with new virtualenv
+
+`pytest -vv --cov=mt_jwt_authentification.jwt_user mt_jwt_authentification` - run tests with pytest-cov (coverage) plugin - configure pyenv first
+
+
+links:
+
+[Pytest](https://github.com/pytest-dev/pytest)
+
+[tox + pytest](https://tox.readthedocs.io/en/latest/example/pytest.html)
+
+[DRF APIClient](http://www.django-rest-framework.org/api-guide/testing/)
+
+[Doctest integration](https://docs.pytest.org/en/latest/doctest.html)
+
+[Pytest Django](https://pytest-django.readthedocs.io/en/latest/)
---
+++ {{ cookiecutter.project_slug }}/setup.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+from pip.req import parse_requirements
+
+install_reqs = parse_requirements('requirements.txt', session=False)
+install_reqs_test = parse_requirements('requirements_dev.txt', session=False)
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [str(ir.req) for ir in install_reqs]
+
+setup_requirements = [
+    # TODO(pss): put setup requirements (distutils extensions, etc.) here
+]
+
+test_requirements = [
+    # TODO: put package test requirements here
+    str(ir.req) for ir in install_reqs_test
+]
+
+{%- set license_classifiers = {
+    'MIT license': 'License :: OSI Approved :: MIT License',
+    'BSD license': 'License :: OSI Approved :: BSD License',
+    'ISC license': 'License :: OSI Approved :: ISC License (ISCL)',
+    'Apache Software License 2.0': 'License :: OSI Approved :: Apache Software License',
+    'GNU General Public License v3': 'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+} %}
+
+setup(
+    name='{{ cookiecutter.project_slug }}',
+    version='{{ cookiecutter.version }}',
+    description="{{ cookiecutter.project_short_description }}",
+    long_description=readme + '\n\n' + history,
+    author="{{ cookiecutter.full_name.replace('\"', '\\\"') }}",
+    author_email='{{ cookiecutter.email }}',
+    url='https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.project_slug }}',
+    packages=find_packages(include=['{{ cookiecutter.aaps_folder_name }}']),
+    include_package_data=True,
+    install_requires=requirements,
+    license="{{ cookiecutter.open_source_license }}",
+    zip_safe=False,
+    keywords='{{ cookiecutter.project_slug }}',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+{%- if cookiecutter.open_source_license in license_classifiers %}
+        '{{ license_classifiers[cookiecutter.open_source_license] }}',
+{%- endif %}
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements,
+    setup_requires=setup_requirements,
+)
